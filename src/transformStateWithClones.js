60cd3cbe300935fb0b2315397a76d2e17ad290df
Josias <josiasonesokataborda22@gmail.com>
2025-10-01 13:51:10 -0300

    funcao tranformStateWithClones.js

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,26 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let nextState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      nextState = { ...nextState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      nextState = { ...nextState };
+
+      for (const key of action.keysToRemove) {
+        delete nextState[key];
+      }
+    } else if (action.type === 'clear') {
+      nextState = {};
+    }
+
+    history.push({ ...nextState });
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
